eb75a26c775165deace2472636c35db88dd2d493
MaksimMarynau <oliverknightbel@gmail.com>
2021-02-28 20:13:29 +0100

    Added postgresql, first app

---
+++ src/coffee_shop_project/coffee_shop_project/apps/coffee_board/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoffeeBoardConfig(AppConfig):
+    name = 'coffee_board'
--- src/coffee_shop_project/coffee_shop_project/settings.py
+++ src/coffee_shop_project/coffee_shop_project/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os, sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kkhx!^6!xbzsfaoox$g4gumo8q5cwecfn*h1f4c%c7wj(ba1&j'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'coffee_shop_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coffee_shop',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD' : os.getenv('DB_PASS'),
     }
 }
 
