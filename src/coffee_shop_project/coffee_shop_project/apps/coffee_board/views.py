80bab284f237eec0dc7cfab42acad51994386450
MaksimMarynau <oliverknightbel@gmail.com>
2021-03-06 18:25:36 +0100

    added view class, created urls, templates and static

--- src/coffee_shop_project/coffee_shop_project/apps/coffee_board/views.py
+++ src/coffee_shop_project/coffee_shop_project/apps/coffee_board/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+from .models import Product
+
+
+class ProductView(ListView):
+    model = Product
+    queryset = Product.objects.filter(status='published').order_by('-publish')
+    paginate_by = 3
+    template_name = 'products/product_list.html'
+    context_object_name = 'my_products'
--- src/coffee_shop_project/coffee_shop_project/settings.py
+++ src/coffee_shop_project/coffee_shop_project/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
