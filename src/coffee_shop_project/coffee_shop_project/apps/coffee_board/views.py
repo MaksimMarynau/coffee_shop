480e3b00f454b0e9b4c4655ea49190f6b0ffdf9d
MaksimMarynau <oliverknightbel@gmail.com>
2021-03-30 20:47:44 +0200

    updated comment system in posts

--- src/coffee_shop_project/coffee_shop_project/apps/coffee_board/views.py
+++ src/coffee_shop_project/coffee_shop_project/apps/coffee_board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
@@ -18,3 +18,22 @@ class ProductDetailView(DetailView):
     model = Product
     slug_field = 'slug'
     template_name = 'products/product_detail.html'
+
+    def post(self, request, slug):
+        form = CommentForm(request.POST)
+        product = Product.objects.get(slug=slug)
+        new_comment = None
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.product = product
+            new_comment.save()
+        return redirect(product.get_absolute_url())
+
+    def get(self, request, slug):
+        form = CommentForm()
+        product = Product.objects.get(slug=slug)
+        return render(request, 'products/product_detail.html',
+            {
+            'form':form,
+            'product':product,
+            })
