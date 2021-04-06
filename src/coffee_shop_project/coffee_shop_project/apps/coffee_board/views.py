e0cf0e65ed9a3bcd2e3fc79f38eb1f5ba95c114b
MaksimMarynau <oliverknightbel@gmail.com>
2021-04-06 19:14:34 +0200

    added pagination to site

--- src/coffee_shop_project/coffee_shop_project/apps/coffee_board/views.py
+++ src/coffee_shop_project/coffee_shop_project/apps/coffee_board/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
+
 # Create your views here.
 from .models import Product, Comment
 from .forms import CommentForm
@@ -9,7 +10,7 @@ from .forms import CommentForm
 class ProductView(ListView):
     model = Product
     queryset = Product.objects.filter(status='published').order_by('-publish')
-    paginate_by = 3
+    paginate_by = 2
     template_name = 'products/product_list.html'
     context_object_name = 'my_products'
 
