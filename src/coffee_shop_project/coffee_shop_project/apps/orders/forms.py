c57250632364389ecfcf1295e9ac0e76580e2c29
MaksimMarynau <oliverknightbel@gmail.com>
2021-10-19 21:14:18 +0200

    changing style and backgrounds

--- src/coffee_shop_project/coffee_shop_project/apps/orders/forms.py
+++ src/coffee_shop_project/coffee_shop_project/apps/orders/forms.py
@@ -8,8 +8,3 @@ class OrderCreateForm(forms.ModelForm):
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address',
                   'postal_code', 'city']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields:
-            self.fields[field].label = False
